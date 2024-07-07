a61cdd5012744cebe14a4a33d3ccee15abf17a3e
flAks <83161350+SnrFlaks@users.noreply.github.com>
2024-07-07 19:20:37 +0300

    added ability to hide range
    also more adaptable for mobile phones

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button/Button";
+export { default as Input } from "./Input/Input";
 export { default as LogoTitle } from "./LogoTitle/LogoTitle";
 export { default as Modal } from "./Modal/Modal";
 export { default as RangeSlider } from "./RangeSlider/RangeSlider";
