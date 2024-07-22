b108904feccbcd62949a49a9390df767514e232e
flAks <83161350+SnrFlaks@users.noreply.github.com>
2024-07-22 17:13:40 +0300

    made basic content for main page
    + improved adaptability for mobile devices

--- src/components/UI/Table/Table.tsx
+++ src/components/UI/Table/Table.tsx
@@ -4,11 +4,12 @@ import styles from "./Table.module.css";
 interface TableProps {
   thead?: React.ReactNode;
   tbody: React.ReactNode;
+  className?: string;
 }
 
-const Table: React.FC<TableProps> = ({ thead, tbody }) => {
+const Table: React.FC<TableProps> = ({ thead, tbody, className }) => {
   return (
-    <table className={styles.table}>
+    <table className={`${styles.table} ${className ? className : ""}`}>
       {thead && <thead>{thead}</thead>}
       <tbody>{tbody}</tbody>
     </table>
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Main.module.css";
+import { Country, countryService } from "@api/CountryService";
+import { Table } from "@components/UI";
 
 interface MainProps {}
 
 const Main: React.FC<MainProps> = () => {
-  return <div className="main"></div>;
+  const [countries, setCountries] = useState<Country[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loadCountries = async () => {
+      const filteredCountries = await countryService.getFilteredCountries({
+        independent: true,
+        unMember: true,
+      });
+      const sortedCountries = await countryService.getSortedCountries(
+        filteredCountries,
+        "name",
+        "asc"
+      );
+      setCountries(sortedCountries);
+    };
+
+    loadCountries();
+  }, []);
+
+  const handleCountryClick = (cca2: string) => {
+    navigate(`/${cca2.toLowerCase()}`);
+  };
+
+  return (
+    <div className={styles.mainContainer}>
+      <h1 className={styles.title}>
+        Welcome to Nation<span className={styles.hub}>Hub</span>
+      </h1>
+      <div className={styles.mainContent}>
+        <Table
+          className={styles.contentTable}
+          thead={
+            <tr>
+              <th>Country</th>
+              <th>Population</th>
+              <th>GDP</th>
+              <th>GDP Per Capita</th>
+            </tr>
+          }
+          tbody={countries.map(({ cca2, name, flagSvg, population, gdp, gdpPCAP }) => (
+            <tr key={cca2}>
+              <td className={styles.countryName} onClick={() => handleCountryClick(cca2)}>
+                <img
+                  src={`data:image/svg+xml,${encodeURIComponent(flagSvg)}`}
+                  alt={`Flag of ${name}`}
+                  className={styles.countryFlag}
+                />
+                {name}
+              </td>
+              <td>{population.value !== null ? Math.round(population.value) : "N/A"}</td>
+              <td>{gdp.value !== null ? `$${Math.round(gdp.value)}` : "N/A"}</td>
+              <td>{gdpPCAP.value !== null ? `$${Math.round(gdpPCAP.value)}` : "N/A"}</td>
+            </tr>
+          ))}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
